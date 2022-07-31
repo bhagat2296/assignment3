de6c8ba888574350b513e7b0f9a1b34ae4be2c71
bhagat2296 <103653090+bhagat2296@users.noreply.github.com>
2022-07-31 20:05:51 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,261 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /*int x;
+    printf("Enter a number\n"); //Question 1
+    scanf("%d",&x);
+    if(x>0)
+        printf("number is possitive");
+    else
+        printf("number is negative");*/
+
+      /*int x;
+    printf("Enter a number\n"); //Question 2
+    scanf("%d",&x);
+    if(x%5)
+        printf("number is not divisible by 5");
+    else
+        printf("number is divisible by 5");*/
+
+
+        /*int x;
+    printf("Enter a number\n"); //Question 3
+    scanf("%d",&x);
+    if(x%2)
+        printf("number is odd");
+    else
+        printf("number is even");*/
+
+    /*int x;
+    printf("Enter a number\n"); //Question 4
+    scanf("%d",&x);
+    if((x/2)*2==x)
+        printf("number is even");
+    else
+        printf("number is odd");*/
+
+
+    /*int x;
+    printf("Enter a number\n"); //Question 5
+    scanf("%d",&x);
+    if(x>99&x<1000)
+        printf("number is 3 digit ");
+    else
+        printf("number is not 3 digit");*/
+
+
+    /*int x,y;
+    printf("Enter a number\n"); //Question 6
+    scanf("%d",&x);
+    printf("Enter a number\n");
+    scanf("%d",&y);
+    if(x!=y)
+    {
+        if(x>y)
+            printf("number %d is greater than %d",x,y);
+    else
+        printf("number %d is greater than %d",y,x);
+
+    }
+    else
+        printf("both number is same");*/
+
+
+      /*int x;
+    printf("Enter a year\n"); //Question 8
+    scanf("%d",&x);
+
+        if(x%4&x%400)
+        printf("enterd year is not a leap year");
+        else
+        printf("entered year is a leap year");*/
+
+
+        /*int x,y,z;
+    printf("Enter first number\n"); //Question 9
+    scanf("%d",&x);
+    printf("Enter second number\n");
+    scanf("%d",&y);
+    printf("Enter third number\n");
+    scanf("%d",&z);
+
+        if(x!=y&y!=z)
+        {
+        if(x>y&x>z)
+        printf("%d number is greatest among 3's\n",x);
+        else if(y>x&y>z)
+        printf("%d number is greatest among 3's\n",y);
+        else if(z>x&z>y)
+
+        printf("%d number is greatest among 3's ",z);
+        }
+        else
+
+            printf("all are equals");*/
+
+
+
+
+    /*float cp,sp;
+    printf("Enter cost price of the item\n"); //Question10
+    scanf("%f",&cp);
+    printf("Enter selling price of the item\n");
+    scanf("%f",&sp);
+
+
+
+    if(sp>cp)
+    {
+        float profit= ((sp-cp)/cp)*100;
+
+        printf("%0.2f percent profit\n",profit);
+
+    }
+
+    else
+    {
+        float loss=(cp-sp)/cp*100;
+        printf("%0.2f percent loss\n",loss);
+    }*/
+
+    /*float sub1,sub2,sub3,sub4,sub5;
+    printf("Enter the number of first subject\n"); //Question11
+    scanf("%f",&sub1);
+    printf("Enter the number of seccond subject\n");
+    scanf("%f",&sub2);
+
+    printf("Enter the number third subject\n");
+    scanf("%f",&sub3);
+
+    printf("Enter the number of fourth subject\n");
+    scanf("%f",&sub4);
+
+    printf("Enter the number fifth subject\n");
+    scanf("%f",&sub5);
+
+     if(sub1>=33&sub2>=33&sub3>=33&sub4>=33&sub5>=33)
+        printf("passed the examintion");
+    else
+
+     printf(" Failed the examination\n");*/
+
+
+
+
+
+     /*char x; //Question 12
+     printf("enter a character\n");
+     scanf("%c",&x);
+
+    if(x>=65&x<=90)
+        printf("Entered character is in uppercase");
+    else if(x>=97&x<=122)
+        printf("Entered number is in lowercase");*/
+
+
+        /*int x;// Question 13
+
+        printf("enter a number");
+        scanf("%d",&x);
+
+        if(x%6)
+            printf("number is not divisible by 2 an 3");
+        else
+            printf("number is divisible by 2 and 3");*/
+
+
+
+           /* int x;// Question 14
+
+        printf("enter a number\n");
+        scanf("%d",&x);
+
+        if(x%3==0|x%7==0)
+            printf("number is  divisible by 7 or 3");
+        else
+            printf("number is not divisible by 7 or 3");*/
+
+
+            /*int x;// Question 15
+
+        printf("enter a number\n");
+        scanf("%d",&x);
+
+        if(x>0)
+            printf("Enterd number is  possitive");
+        else if(x<0)
+            printf("Enterd number is negative");
+            else
+                printf("number is zero");*/
+
+
+
+
+
+    /*char x; //Question 16
+     printf("enter a character\n");
+     scanf("%c",&x);
+
+    if(x>=65&x<=90)
+        printf("Entered character is in uppercase");
+
+    else if(x>=97&x<=122)
+        printf("Entered number is in lowercase");
+
+    else if(x>=48&x<=57)
+        printf("entered number is a digit");
+
+    else
+        printf("entered number is a special character");*/
+
+
+
+        /*int s1,s2,s3;// Question 17
+
+        printf("enter dimension of side 1\n");
+        scanf("%d",&s1);
+
+        printf("enter dimension of side 2\n");
+        scanf("%d",&s2);
+
+        printf("enter dimension of side 3\n");
+        scanf("%d",&s3);
+
+        if(s1+s2>s3&s2+s3>s3&s1+s3>s2)
+
+            printf("triangle is valid");
+        else
+            printf("triangle is not valid");*/
+
+
+
+
+            /*int x;
+            printf("enter the month\n");
+            scanf("%d",&x);
+
+            if(x==1|x==3|x==5|x==7|x==8|x==10|x==12)
+
+                printf("31 days");
+
+            else if(x==4|x==6|x==9|x==11)
+
+                printf("30 days");
+
+                else
+            printf("29 or 30 days");*/
+
+
+
+
+
+            //Question 7 has not solved
+
+
+
+
+
+    return 0;
+}
